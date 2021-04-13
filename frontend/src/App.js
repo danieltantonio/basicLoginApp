d6f76517265a95db58393028954aafabd310a024
Daniel Antonio <danieltomantonio@gmail.com>
2021-04-12 18:21:37 -0700

    Added onChange functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const initLogin = {
+    email: '',
+    passwd: ''
+}
+
 const App = () => {
+    const [login, setLogin] = useState(initLogin);
+
+    const formChange = e => {
+        const {name, value} = e.target;
+        setLogin({ ...login, [name]: value });
+    }
+
+    const sub = e => {
+        e.preventDefault();
+        alert('it works');
+    }
+
     return(
         <div>
             <h1 id="login-header">Login</h1>
-            <form>
-                <input type="email" placeholder="Email"></input>
-                <input type="password" placeholder="Password"></input>
+            <form onSubmit={sub}>
+                <input type="email" name="email" value={login.email} onChange={formChange} placeholder="Email"></input>
+                <input type="password" name ="passwd" value={login.passwd} onChange={formChange} placeholder="Password"></input>
                 <button type="submit">Login</button>
             </form>
         </div>
